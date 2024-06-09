6fe71f89c04b316fef2a9a7d7f9e21d9fb64eaa0
Julien Etelain <julien.etelain@outlook.com>
2024-06-09 19:26:44 +0200

    In memory storage for easing unit testing

---
+++ HugeImages/Storage/MemoryHugeImageStorage.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Concurrent;
+
+namespace HugeImages.Storage
+{
+    /// <summary>
+    /// In-Memory storage intend for unit testing
+    /// </summary>
+    public sealed class MemoryHugeImageStorage : IHugeImageStorage, IDisposable
+    {
+        public ConcurrentDictionary<string, MemoryHugeImageStorageSlot> Slots { get; } = new ConcurrentDictionary<string, MemoryHugeImageStorageSlot>();
+
+        public IHugeImageStorageSlot CreateSlot(string name, HugeImageSettingsBase settings)
+        {
+            return Slots.GetOrAdd(name, n => new MemoryHugeImageStorageSlot(n, settings));
+        }
+
+        public void Dispose()
+        {
+            foreach(var value in Slots.Values)
+            {
+                value.Dispose();
+            }
+            Slots.Clear();
+        }
+    }
+}
